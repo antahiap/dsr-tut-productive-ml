1294434c08dfc46a9346a8ecf699d6cc778903aa
antahiap <antahia@yahoo.com>
2023-08-15 16:45:50 +0200

    deployment of model

---
+++ iris_app.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI, Request
+from joblib import load
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Prediction(BaseModel):
+    prediction: int 
+
+@app.get("/")
+def root():
+    return {"message": "Hello World"}
+
+@app.get("/predict", response_model=Prediction)
+def predict(req:Request):
+
+    query_params =  req.query_params
+
+    data = [
+        query_params['sequallength'],
+        query_params['sequalwidth'],
+        # query_params['petallength'],
+        # query_params['petalwidth']
+    ]
+
+    # call the model
+    clf = load('model/iris_hp_model.joblib')
+    pred = clf.predict([data])
+
+    return {'prediction': int(pred)}
